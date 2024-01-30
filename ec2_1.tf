617e6925ddfeeb5390a518465680a45228e66ac2
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-30 04:47:07 +0100

    to create first demo virtual server in aws

---
+++ ec2_1.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAQTVLCTWLD6U4YFVG"
+  secret_key = "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"
+}
+
+resource "aws_instance" "demo_ec2" {
+    ami = "ami-0c7217cdde317cfec"
+    instance_type = "t2.micro"
+}
